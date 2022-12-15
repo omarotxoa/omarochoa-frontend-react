1b9eae4921e5b69f54c38cda12a471561233b124
Omar Ochoa <omar@ochoawebdesign.com>
2022-12-14 18:04:03 -0800

    Creates Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <Nav />
+    <>
+      <Nav />
+      <Hero />
+    </>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <div>
+      <h1>Omar Ochoa</h1>
+      <p>Web Developer</p>
+      <p>I focus on the Front End, having a particular love for CSS and static site generators.  I love template-ing, theme-ing,  and organizing. With a past in Graphic Design, SEM, and Project Management - my experience has garnered me a mix of soft and hard skills with which I give my all to the work. </p>
+    </div>
+  );
+}
+
+export default Hero;
