e688103adee6be57ebd9afc91d08b8b4e862acd3
Omar Ochoa <omar@ochoawebdesign.com>
2023-02-09 17:37:54 -0800

    Creates the Tools component with first draft copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import Tools from "./components/Tools";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Tools />
     </>
   )
 }
---
+++ src/components/Tools.jsx
@@ -0,0 +1,22 @@
+function Tools() {
+  return(
+    <section>
+      <h2>Tools of the Trade</h2>
+      <p>I am not loyal to any particular framework/library or language. There is no one tech stack to rule them all. There is the websites purpose, the clients needs and goals, and the process of choosing the appropriate tech for that given website - then working that to completion. Here are languages, frameworks, and tools I've been working with recently:</p>
+      <ul>
+        <li>HTML5</li>
+        <li>CSS3 / SASS</li>
+        <li>JavaScript</li>
+        <li>PHP</li>
+        <li>MySQL</li>
+        <li>Wordpress (theme/plugins/headless)</li>
+        <li>ReactJS</li>
+        <li>Hugo / Jekyll</li>
+        <li>Vite / Gulp / Webpack / Babel</li>
+        <li>Photoshop / Figma / Sketch</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Tools;
