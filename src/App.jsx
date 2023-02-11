5199d7ae90b44e78d99b48750a3d8e2423c0e88d
Omar Ochoa <omar@ochoawebdesign.com>
2023-02-10 16:48:34 -0800

    Creates the Contact component with first draft copy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Tools from "./components/Tools";
 import Experience from "./components/Experience";
 import WorkPhilosophy from "./components/WorkPhilosophy";
 import SelectedWorks from "./components/SelectedWorks";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Experience />
       <WorkPhilosophy />
       <SelectedWorks />
+      <Contact />
     </>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+  return(
+    <section>
+      <h2>Contact</h2>
+      <p>Resonate with anything I've said? Great! I'd love to work with you. Feel free to say hi.</p>
+      <button>Get in Touch</button>
+    </section>
+  );
+}
+
+export default Contact;
